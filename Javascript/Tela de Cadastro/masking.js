95f092502e21db11248b3f54c8d3f7010a518683
saragamati <110948567+saragamati@users.noreply.github.com>
2023-02-16 19:39:04 -0300

    Add files via upload

---
+++ Javascript/Tela de Cadastro/masking.js
@@ -0,0 +1,11 @@
+function onlyLetters(k){
+    if(!(k.keyCode >= 65 && k.keyCode <= 90) && !(k.keyCode == 186) && !(k.keyCode == 8)){
+        k.preventDefault();
+    }
+}
+
+function onlyNumbers(k){
+    if(!(k.keyCode >= 48 && k.keyCode <= 57) && !(k.keyCode >= 96 && k.keyCode <= 105) && !(k.keyCode == 8)){
+        k.preventDefault();
+    }
+}
--- Javascript/Tela de Cadastro/script.js
+++ Javascript/Tela de Cadastro/script.js
@@ -1,7 +1,15 @@
 function abrirCamposCadastro(){
     var formsField = document.getElementById('FormsField');
-
-    formsField.classList.remove("FormsFieldContainer")
+    var listFormsField = formsField.classList;
+    
+    if(listFormsField.contains("FormsFieldContainer_hidden") == true){
+        formsField.classList.remove("FormsFieldContainer_hidden");
+        formsField.classList.add("FormsFieldContainer_show");
+    }else{
+        formsField.classList.remove("FormsFieldContainer_show");
+        formsField.classList.add("FormsFieldContainer_hidden");
+    }
+    
 }
 
 function createCadastroRow(){
@@ -31,7 +39,7 @@ function createCadastroRow(){
     editIcon.setAttribute("class","bi bi-pencil");
     
     editButton.setAttribute("class","btn btn-primary");
-    editButton.setAttribute("onclick","showEditFields(this)");
+    editButton.setAttribute("onclick","transferValueToModal(this)");
     editButton.setAttribute("data-bs-toggle","modal");
     editButton.setAttribute("data-bs-target","#staticBackdrop");
     editButton.innerHTML = editIcon.outerHTML;
@@ -50,17 +58,59 @@ function createCadastroRow(){
     deleteButton.innerHTML = deleteIcon.outerHTML;
 
     iconsCell.innerHTML += deleteButton.outerHTML;
+
+    var formNameEle = document.getElementById('createNameField');
+    var formAgeEle = document.getElementById('createIdadeField');
+    formNameEle.value = "";
+    formAgeEle.value = "";
+
+    var formsField = document.getElementById('FormsField');
+    formsField.classList.remove("FormsFieldContainer_show");
+    formsField.classList.add("FormsFieldContainer_hidden");
 }
 
-function showEditFields(btn){
+function transferValueToModal(btn){
+    //removendo todas as TR em edição, caso haja
+    var editingElements = document.querySelectorAll("tr.editing");
+    for(var pos=0;pos<=((editingElements.length)-1);pos++){
+        editingElements[pos].classList.remove('editing');
+    }
+
     var currentRow = btn.closest("tr");
+    currentRow.classList.add("editing");
+
     var TDElements = currentRow.cells;
 
-    //salvando os dados antigos em variáveis
     var currentName = TDElements[0].innerHTML;
     var currentAge = TDElements[1].innerHTML;
     var currentGender = TDElements[2].innerHTML;
 
+    var editNameEle = document.getElementById("editNameField");
+    var editAgeEle = document.getElementById("editIdadeField");
+    var editGenderEle = document.getElementById("editGenderField");
 
-    console.log(currentName, currentAge, currentGender);
+    editNameEle.value = currentName;
+    editAgeEle.value = currentAge;
+    editGenderEle.value = currentGender;
+}
+
+function alterarCadastro(){
+    var newFormName = document.getElementById('editNameField').value;
+    var newFormAge = document.getElementById('editIdadeField').value;
+    var newformGender = document.getElementById('editGenderField').value;
+    var editingRow = document.getElementsByClassName('editing');
+    var editingCells = editingRow[0].cells;
+
+    editingCells[0].innerHTML = newFormName;
+    editingCells[1].innerHTML = newFormAge;
+    editingCells[2].innerHTML = newformGender;
+
+    editingRow[0].classList.remove('editing');
+}
+
+function deleteRow(btn){
+    if(confirm("Tem certeza que deseja deletar este cadastro?") == true){
+        var currentRow = btn.closest("tr");
+        currentRow.remove();
+    }
 }
